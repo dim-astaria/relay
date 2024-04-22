41c654dc72e1bfed1c75d2ca98ba75a66db34d4a
dim-ser <dim-s@datasub.com>
2024-04-23 00:20:16 +0300

    fix: redis issue

--- src/http.ts
+++ src/http.ts
@@ -53,14 +53,10 @@ export class HttpService {
     this.logger = generateChildLogger(logger, this.context);
     this.metrics = this.setMetrics();
     this.redis = new RedisService(this, this.logger);
-    this.redis.initialize().then(() => {
-      console.log('REDIS INITIALIZED!!!')
-      this.initialize();
-      this.ws = new WebSocketService(this, this.logger);
-      this.message = new MessageService(this, this.logger);
-      this.subscription = new SubscriptionService(this, this.logger);
-      this.notification = new NotificationService(this, this.logger);
-    });
+    this.ws = new WebSocketService(this, this.logger);
+    this.message = new MessageService(this, this.logger);
+    this.subscription = new SubscriptionService(this, this.logger);
+    this.notification = new NotificationService(this, this.logger);
   }
 
   public on(event: string, listener: any): void {
@@ -79,9 +75,9 @@ export class HttpService {
     this.events.removeListener(event, listener);
   }
 
-  // ---------- Private ----------------------------------------------- //
 
-  private initialize(): void {
+  public async initialize(): Promise<void> {
+    await this.redis.initialize();
     this.logger.trace(`Initialized`);
     this.registerApi();
     this.setBeatInterval();
@@ -121,6 +117,7 @@ export class HttpService {
     }
   }
 
+  // ---------- Private ----------------------------------------------- //
   private registerApi() {
     this.app.register(helmet);
     this.app.register(ws);
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import { HttpService } from "./http";
 
 const { app } = new HttpService(config);
 
-app.listen({ port, host }, async (err, address) => {
-  app.log.info(`Server listening on ${address}`);
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-});
+const run = async () => {
+  await HttpService.initialize();
+
+  app.listen({ port, host }, async (err, address) => {
+    app.log.info(`Server listening on ${address}`);
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+  });
+}
+
+run();
